a0346f458fd00adceffa94f6fe27421f3bca7a59
TheLike-beep-boop <66366163+TheLike-beep-boop@users.noreply.github.com>
2026-07-27 21:34:02 +0300

    app.js

--- app.js
+++ app.js
@@ -47,10 +47,10 @@ function updateValidityBadge() {
     today.setHours(0, 0, 0, 0);
 
     if (today > expDate) {
-        status.innerHTML = '<i class="fa-solid fa-xmark"></i> Stingator Expirat';
+        status.innerHTML = '<i class="fa-solid fa-xmark"></i> Stingător Invalid';
         setBoxColor("#D4121A");
     } else {
-        status.innerHTML = '<i class="fa-solid fa-check"></i> Stingator Valid';
+        status.innerHTML = '<i class="fa-solid fa-check"></i> Stingător Valid';
         setBoxColor("#51C323");
     }
 }
